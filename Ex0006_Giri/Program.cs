68363f4974bc85fbd83f60b00cc9f19774ede11b
evgeny_ganin <ufolklore@yandex.ru>
2023-01-19 23:37:10 +0300

    Push Ex0006_Giri

--- Ex0006_Giri/Program.cs
+++ Ex0006_Giri/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int a = 1;
+int b = 8;
+int c = 1985;
+int d = 20;
+int e = 6;
+int max = a;
+if (b > max) max = b ;
+if (c > max) max = c ;
+if (d > max) max = d ;
+if (e > max) max = e ;
+// System.Console.Write(max);
+System.Console.Write("Вес самой тяжелой гири = ");
+System.Console.Write(max);
+System.Console.Write(" кг");
