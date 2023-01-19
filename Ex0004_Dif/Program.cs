1bd5a4a319cc4d23a8d8ad3630a7cf51766338a1
evgeny_ganin <ufolklore@yandex.ru>
2023-01-19 23:21:15 +0300

    Add Ex0004_Dif

---
+++ Ex0004_Dif/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+//Деление целых чисел
+int num1 = 160;
+int num2 = 6;
+Console.WriteLine(num1 / num2);
+
+//Деление нормальное
+double num1 = 160;
+double num2 = 6;
+Console.WriteLine(num1 / num2);
+
+// Вспомогательные конструкции
+// int - целые числа 
+// double - дробные числа
+// string - строка
+// bool - ложь и истина (true, false)
+
+// //Случайности
+// new Random().Next(min, max);
