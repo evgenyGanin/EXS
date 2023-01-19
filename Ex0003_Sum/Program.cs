e6257597e48c36985eb5cdab0b9d28315a2b9130
evgeny_ganin <ufolklore@yandex.ru>
2023-01-19 23:06:06 +0300

    Add Ex0003_Sum folder

---
+++ Ex0003_Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+// //Сумма 2х чисел
+int num1 = 37;
+int num2 = 45;
+System.Console.WriteLine(num1 + num2);
+
+//Сумма 2х чисел через промежуточную переменную
+int num1 = 3;
+int num2 = 52;
+int result = num1 + num2; //дополнительный промежуточный контейнер
+System.Console.WriteLine(result);
+
+//Сумма 2х СЛУЧАЙНЫХ ЧИСЕЛ через промежуточную переменную
+int num1 = new Random().Next(1, 10);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
+int result = num1 + num2; //дополнительный промежуточный контейнер
+Console.WriteLine(result);
+
+
+//Test
+int num1 = new Random().Next(1, 10);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
+int num3 = new Random().Next(1, 10);
+Console.WriteLine(num3);
+int result = num2;
+// int result = (num1 + num2 + num3) - (num1 + num3); //дополнительный промежуточный контейнер
+Console.WriteLine(result);
