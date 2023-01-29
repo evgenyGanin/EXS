3e538063a46269c35ff5734977e0747bd0db14ac
evgeny_ganin <ufolklore@yandex.ru>
2023-01-29 19:09:49 +0300

    Add S4 folder

---
+++ S4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+void arrayFill()
+{
+    for (int i = 0; i < array.Length; i++);
+    {
+        array[i] = rand.Next(-10, 11);
+    }
+
+void arrayFill()
+printArray(array);
+
+int[] array = new int[4];
+Random rand = new Random();
